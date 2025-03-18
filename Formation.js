94b1e76c460cf903b87f7a805843ae1b079494da
Thorsten <thorsten.mueller@mailbox.org>
2025-03-18 13:55:03 +0100

    typo

--- Formation.js
+++ Formation.js
@@ -60,97 +60,97 @@ function handleTicker(sTicker, sTimeFrame){
 	})
 }
 
-// const runnerM15 = cron.schedule('0,15,30,45 * * * *', async () => { 
+const runnerM15 = cron.schedule('0,15,30,45 * * * *', async () => { 
 	
-// 	await utils.ntfyMe('Log', `crone running BTC-M15 formationscript`)
-// 	for await (const sTicker of aTicker) {
-// 		await handleTicker(sTicker, '15m')
-// 	}
-// })
-// const runnerM30 = cron.schedule('0,30 * * * *', async () => { 
+	await utils.ntfyMe('Log', `crone running BTC-M15 formationscript`)
+	for await (const sTicker of aTicker) {
+		await handleTicker(sTicker, '15m')
+	}
+})
+const runnerM30 = cron.schedule('0,30 * * * *', async () => { 
 	
-// 	await utils.ntfyMe('Log', `crone running BTC-M30 formationscript`)
-// 	for await (const sTicker of aTicker) {
-// 		await handleTicker(sTicker, '30m')
-// 	}
-// })
-// const runnerH1 = cron.schedule('* */1 * * *', async () => { 
+	await utils.ntfyMe('Log', `crone running BTC-M30 formationscript`)
+	for await (const sTicker of aTicker) {
+		await handleTicker(sTicker, '30m')
+	}
+})
+const runnerH1 = cron.schedule('* */1 * * *', async () => { 
 	
-// 	await utils.ntfyMe('Log', `crone running BTC-H1 formationscript`)
-// 	for await (const sTicker of aTicker) {
-// 		await handleTicker(sTicker, '30m')
-// 	}
-// })
-// const runnerH2 = cron.schedule('1 */2 * * *', async () => { 
+	await utils.ntfyMe('Log', `crone running BTC-H1 formationscript`)
+	for await (const sTicker of aTicker) {
+		await handleTicker(sTicker, '1H')
+	}
+})
+const runnerH2 = cron.schedule('1 */2 * * *', async () => { 
 	
-// 	await utils.ntfyMe('Log', `crone running BTC-H2 formationscript`)
-// 	for await (const sTicker of aTicker) {
-// 		await handleTicker(sTicker, '30m')
-// 	}
-// })
+	await utils.ntfyMe('Log', `crone running BTC-H2 formationscript`)
+	for await (const sTicker of aTicker) {
+		await handleTicker(sTicker, '2H')
+	}
+})
 
-// const runnerH4 = cron.schedule('1 0,4,8,12,16,20 * * *', async () => { 
-// // minute 2 of hour 01, 05, 09, 13, 17 and 21
+const runnerH4 = cron.schedule('1 0,4,8,12,16,20 * * *', async () => { 
+// minute 2 of hour 01, 05, 09, 13, 17 and 21
 	
-// 	await utils.ntfyMe('Log', `crone running BTC-H4 formationscript`)
-// 	for await (const sTicker of aTicker) {
-// 		await handleTicker(sTicker, '30m')
-// 	}
-// })
+	await utils.ntfyMe('Log', `crone running BTC-H4 formationscript`)
+	for await (const sTicker of aTicker) {
+		await handleTicker(sTicker, '4H')
+	}
+})
 
 
 
-// runnerM15.start() 
-// runnerM30.start() 
-// runnerH1.start()
-// runnerH2.start()
-// runnerH4.start()
+runnerM15.start() 
+runnerM30.start() 
+runnerH1.start()
+runnerH2.start()
+runnerH4.start()
 
 
-function testFormation(sTicker, sTimeFrame){
-	return new Promise((resolve, reject) => {
-		bitget.getTickerData(`${sTicker}USDT`, sTimeFrame, '10')
-		.then(async (aData) => {
+// function testFormation(sTicker, sTimeFrame){
+// 	return new Promise((resolve, reject) => {
+// 		bitget.getTickerData(`${sTicker}USDT`, sTimeFrame, '10')
+// 		.then(async (aData) => {
 			
-			if(utils.isMCCandle(aData[aData.length - 2], aData[aData.length - 3])){
-				console.log(`MC Candle ${aData[i+1].timestamp}`)
-			}
-			//probably needs a given level to watch for -> otherwise there will be too many alerts
-			if(utils.isLiquidation(aData[aData.length - 2], aData[aData.length - 3], aData[aData.length - 4])){
-				console.log(`Liquidation ${aData[i+1].timestamp}`)
-			}
+// 			if(utils.isMCCandle(aData[aData.length - 2], aData[aData.length - 3])){
+// 				console.log(`MC Candle ${aData[i+1].timestamp}`)
+// 			}
+// 			//probably needs a given level to watch for -> otherwise there will be too many alerts
+// 			if(utils.isLiquidation(aData[aData.length - 2], aData[aData.length - 3], aData[aData.length - 4])){
+// 				console.log(`Liquidation ${aData[i+1].timestamp}`)
+// 			}
 
-			for (let i = aData.length -3; i >= 4 ; i--) {
+// 			for (let i = aData.length -3; i >= 4 ; i--) {
                 
-				if(utils.isInsideOutFormation(aData[i+2], aData[i+1], aData[i])){
-                    console.log(`InsideOut ${aData[i].timestamp}`)
-                }
-				if(utils.isWyckoff(aData[i], i, aData)){
-					//TODO check when available
-					console.log(`Wyckoff ${aData[i].timestamp}`)
-				}
-				if(utils.isEveMorningStar(aData)){
-					//already implemented without retest
-                    console.log(`EveMorningStar ${aData[i].timestamp}`)
-                }
-				//TODO U-V Formation (4Candles)
-				// if(utils.isUFormation(aData[i], i, aData)){
-				// 	console.log(`u/V Formation ${aData[i+1].timestamp}`)
-				// }
+// 				if(utils.isInsideOutFormation(aData[i+2], aData[i+1], aData[i])){
+//                     console.log(`InsideOut ${aData[i].timestamp}`)
+//                 }
+// 				if(utils.isWyckoff(aData[i], i, aData)){
+// 					//TODO check when available
+// 					console.log(`Wyckoff ${aData[i].timestamp}`)
+// 				}
+// 				if(utils.isEveMorningStar(aData)){
+// 					//already implemented without retest
+//                     console.log(`EveMorningStar ${aData[i].timestamp}`)
+//                 }
+// 				//TODO U-V Formation (4Candles)
+// 				// if(utils.isUFormation(aData[i], i, aData)){
+// 				// 	console.log(`u/V Formation ${aData[i+1].timestamp}`)
+// 				// }
 
-            }
-			setTimeout(() => {resolve()}, 1000)
-			})
+//             }
+// 			setTimeout(() => {resolve()}, 1000)
+// 			})
 
-		.catch(err => {
-			console.log(err)
-			setTimeout(() => {reject()}, 1000)
-		})
-	})
-}
+// 		.catch(err => {
+// 			console.log(err)
+// 			setTimeout(() => {reject()}, 1000)
+// 		})
+// 	})
+// }
 
 
-async function main () {
-    await testFormation('BTC', '30m')
-}
-main()
+// async function main () {
+//     await testFormation('BTC', '30m')
+// }
+// main()
