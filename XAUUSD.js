0e51e705fb0713204265e570ae50af3fc75b4801
Thorsten <thorsten.mueller@mailbox.org>
2025-07-19 08:55:29 +0200

    adjust H4 runner

--- XAUUSD.js
+++ XAUUSD.js
@@ -104,16 +104,15 @@ const runnerH1 = cron.schedule('15 0 */1 * * *', async () => {
 	await utils.ntfyMe('Log', `crone running XAUUSD H1 script`)
 	fetchGoldData(url, 'H1');
 })
-//TODO
-// const runnerH4 = cron.schedule('10 0 */4 * * *', async () => { 
-//     // minute 2 of hour 01, 05, 09, 13, 17 and 21
-//     const url = 'https://financialmodelingprep.com/stable/historical-chart/4hour?symbol=XAUUSD&apikey=bmrHqCf73Wic7Arp856mK7v1g2lfa4A8';
+const runnerH4 = cron.schedule('20 0 01,05,09,13,17,21 * * *', async () => { 
+    // minute 2 of hour 01, 05, 09, 13, 17 and 21 //Server is at UTC - exchange at UTC-5
+    const url = 'https://financialmodelingprep.com/stable/historical-chart/4hour?symbol=XAUUSD&apikey=bmrHqCf73Wic7Arp856mK7v1g2lfa4A8';
 	
-// 	await utils.ntfyMe('Log', `crone running XAUUSD H4 script`)
-// 	fetchGoldData(url, 'H4');
-// })
+	await utils.ntfyMe('Log', `crone running XAUUSD H4 script`)
+	fetchGoldData(url, 'H4');
+})
 
 // runnerM15.start()
 runnerM30.start()
 runnerH1.start()
-// runnerH4.start()
+runnerH4.start()
